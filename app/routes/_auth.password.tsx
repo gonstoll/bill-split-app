db9301353dbe37cb3e387cb7253724042f675e20
Gonzalo Stoll <stollgonzalo@gmail.com>
2024-03-08 21:51:25 +0100

    Removing register button from password page
    Landing on the password page without a userId takes you to the register page

--- app/routes/_auth.password.tsx
+++ app/routes/_auth.password.tsx
@@ -12,7 +12,7 @@ import {ErrorList} from '~/components/error-list'
 import {Button} from '~/components/ui/button'
 import {Input} from '~/components/ui/input'
 import {Label} from '~/components/ui/label'
-import {commitSession, destroySession, getSession} from '~/utils/session.server'
+import {commitSession, getSession} from '~/utils/session.server'
 import {knownErrorSchema} from '~/utils/types'
 
 const PasswordSchema = z
@@ -29,23 +29,18 @@ const PasswordSchema = z
     'Passwords must match',
   )
 
-const IntentSchema = z.object({
-  intent: z.union([z.literal('set-password'), z.literal('register')]),
-})
-
 export async function loader({request}: LoaderFunctionArgs) {
   const session = await getSession(request.headers.get('Cookie'))
   const userId = session.get('userId')
   if (!userId) {
-    return redirect('/register', {status: 401})
+    throw redirect('/register')
   }
-  return json({userId: session.get('userId'), email: session.get('email')})
+  return json({userId, email: session.get('email')})
 }
 
 export async function action({request}: ActionFunctionArgs) {
   const session = await getSession(request.headers.get('Cookie'))
   const formData = await request.formData()
-  const intentResult = parseWithZod(formData, {schema: IntentSchema})
   const userId = session.get('userId')
   const email = session.get('email')
 
@@ -53,88 +48,66 @@ export async function action({request}: ActionFunctionArgs) {
     return redirect('/register', {status: 401})
   }
 
-  if (intentResult.status !== 'success') {
+  const result = parseWithZod(formData, {schema: PasswordSchema})
+
+  if (result.status !== 'success') {
+    return json(
+      {
+        type: 'error',
+        result: result.reply({
+          formErrors: ["Passwords don't match"],
+        }),
+      } as const,
+      {status: result.status === 'error' ? 400 : 200},
+    )
+  }
+
+  const body = result.value
+  const response = await fetch(
+    'http://localhost:5003/api/Authorization/password',
+    {
+      method: 'POST',
+      body: JSON.stringify({...body, userId}),
+      headers: {'content-type': 'application/json'},
+    },
+  )
+
+  if (!response.ok) {
+    const parsedError = knownErrorSchema.safeParse(await response.json())
+    if (!parsedError.success) {
+      throw new Response('Invalid response from server', {status: 500})
+    }
+    const {detail} = parsedError.data
     return json(
       {
         status: 'error',
-        result: intentResult.reply(),
+        result: result.reply({formErrors: [detail]}),
       } as const,
-      {status: intentResult.status === 'error' ? 400 : 200},
+      {status: response.status},
     )
   }
 
-  const {intent} = intentResult.value
-
-  if (intent === 'register') {
-    return redirect('/redirect', {
-      headers: {'set-cookie': await destroySession(session)},
-    })
-  }
-
-  if (intent === 'set-password') {
-    const result = parseWithZod(formData, {schema: PasswordSchema})
-
-    if (result.status !== 'success') {
-      return json(
-        {
-          type: 'error',
-          result: result.reply({
-            formErrors: ["Passwords don't match"],
-          }),
-        } as const,
-        {status: result.status === 'error' ? 400 : 200},
-      )
-    }
-
-    const body = result.value
+  // Registration is done, we now perform the login for the user
+  if (response.status === 204) {
     const response = await fetch(
-      'http://localhost:5003/api/Authorization/password',
+      'http://localhost:5003/api/Authorization/login',
       {
         method: 'POST',
-        body: JSON.stringify({...body, userId}),
+        body: JSON.stringify({email, password: body.password}),
         headers: {'content-type': 'application/json'},
       },
     )
+    // TODO: Handle this better
+    const data = (await response.json()) as {token: string; expiresOn: Date}
 
-    if (!response.ok) {
-      const parsedError = knownErrorSchema.safeParse(await response.json())
-      if (!parsedError.success) {
-        throw new Response('Invalid response from server', {status: 500})
-      }
-      const {detail} = parsedError.data
-      return json(
-        {
-          status: 'error',
-          result: result.reply({
-            formErrors: [detail],
-          }),
-        } as const,
-        {status: response.status},
-      )
-    }
+    session.set('token', data.token)
+    session.set('expiresOn', data.expiresOn)
+    session.unset('userId')
+    session.unset('email')
 
-    // Registration is done, we now perform the login for the user
-    if (response.status === 204) {
-      const response = await fetch(
-        'http://localhost:5003/api/Authorization/login',
-        {
-          method: 'POST',
-          body: JSON.stringify({email, password: body.password}),
-          headers: {'content-type': 'application/json'},
-        },
-      )
-      // TODO: Handle this better
-      const data = (await response.json()) as {token: string; expiresOn: Date}
-
-      session.set('token', data.token)
-      session.set('expiresOn', data.expiresOn)
-      session.unset('userId')
-      session.unset('email')
-
-      return redirect('/', {
-        headers: {'set-cookie': await commitSession(session)},
-      })
-    }
+    return redirect('/', {
+      headers: {'set-cookie': await commitSession(session)},
+    })
   }
 }
 
@@ -159,7 +132,7 @@ export default function PasswordForm() {
       </div>
 
       <Form method="post" {...getFormProps(form)}>
-        <div className="flex flex-col gap-2">
+        <div className="mb-4 flex flex-col gap-2">
           <Label htmlFor={fields.password.id}>Password</Label>
           <Input
             {...getInputProps(fields.password, {type: 'password'})}
@@ -167,7 +140,7 @@ export default function PasswordForm() {
           />
           <ErrorList id={fields.password.id} errors={fields.password.errors} />
         </div>
-        <div className="mt-4 flex flex-col gap-2">
+        <div className="mb-4 flex flex-col gap-2">
           <Label htmlFor={fields.passwordCheck.id}>Repeat password</Label>
           <Input {...getInputProps(fields.passwordCheck, {type: 'password'})} />
           <ErrorList
@@ -176,25 +149,9 @@ export default function PasswordForm() {
           />
         </div>
 
-        <div className="mt-4 flex items-center gap-2">
-          <Button
-            className="flex-1"
-            variant="outline"
-            name="intent"
-            value="register"
-            type="submit"
-          >
-            Register
-          </Button>
-          <Button
-            className="flex-1"
-            name="intent"
-            value="set-password"
-            type="submit"
-          >
-            Set password
-          </Button>
-        </div>
+        <Button className="w-full" type="submit">
+          Set password
+        </Button>
       </Form>
     </div>
   )
