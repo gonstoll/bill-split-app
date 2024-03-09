713996ea8f8e5f9437abbc4032dadb46169629fb
Gonzalo Stoll <stollgonzalo@gmail.com>
2024-03-09 23:04:16 +0100

    Responsiveness fix on auth forms

--- app/routes/_auth.tsx
+++ app/routes/_auth.tsx
@@ -2,7 +2,7 @@ import {Outlet} from '@remix-run/react'
 
 export default function AuthLayout() {
   return (
-    <div className="mx-auto mt-6 w-96">
+    <div className="mx-auto mt-6 w-full sm:w-96">
       <Outlet />
     </div>
   )
