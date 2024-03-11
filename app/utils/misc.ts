a3c201e1b9e59840d83cb7a7842899ab4b61b198
Gonzalo Stoll <stollgonzalo@gmail.com>
2024-03-11 16:04:12 +0100

    Api class exploration

--- app/utils/misc.ts
+++ app/utils/misc.ts
@@ -18,14 +18,62 @@ export function getErrorMessage(error: unknown) {
   return 'Unknown Error'
 }
 
+class Api {
+  private headers: Headers
+  constructor(private request: Request) {
+    this.headers = new Headers()
+  }
+
+  async init() {
+    const session = await getSession(this.request.headers.get('Cookie'))
+    const token = session.get('token')
+    if (token) {
+      this.headers.append('authorization', `Bearer ${token}`)
+    } else {
+      this.headers.delete('authorization')
+    }
+  }
+
+  async get(url: string, init?: Omit<RequestInit, 'body'>) {
+    await this.init()
+    return await fetch(`${ENV.BASE_URL}/api/${url}`, {
+      ...init,
+      headers: {
+        'content-type': 'application/json',
+        ...Object.fromEntries(this.headers.entries()),
+        ...init?.headers,
+      },
+    })
+  }
+
+  async post(
+    url: string,
+    body?: Record<string, unknown>,
+    init?: Omit<RequestInit, 'body'>,
+  ) {
+    await this.init()
+    return await fetch(`${ENV.BASE_URL}/api/${url}`, {
+      ...init,
+      method: 'POST',
+      body: body ? JSON.stringify(body) : undefined,
+      headers: {
+        'content-type': 'application/json',
+        ...Object.fromEntries(this.headers.entries()),
+        ...init?.headers,
+      },
+    })
+  }
+}
+
 export const fetcher = serverOnly$(fetcherFn)!
 async function fetcherFn(request?: Request) {
   const session = await getSession(request?.headers.get('Cookie'))
   const token = session.get('token')
   const headers = new Headers()
   if (token) headers.append('authorization', `Bearer ${token}`)
+
   return {
-    async get(url: string, init?: RequestInit) {
+    async get(url: string, init?: Omit<RequestInit, 'body'>) {
       return await fetch(`${ENV.BASE_URL}/api/${url}`, {
         ...init,
         headers: {
@@ -35,11 +83,15 @@ async function fetcherFn(request?: Request) {
         },
       })
     },
-    async post(url: string, body?: unknown, init?: RequestInit) {
+    async post(
+      url: string,
+      body?: Record<string, unknown>,
+      init?: Omit<RequestInit, 'body'>,
+    ) {
       return await fetch(`${ENV.BASE_URL}/api/${url}`, {
         ...init,
         method: 'POST',
-        body: body ? JSON.stringify(body) : undefined,
+        body: body ? JSON.stringify(body) : null,
         headers: {
           'content-type': 'application/json',
           ...Object.fromEntries(headers.entries()),
